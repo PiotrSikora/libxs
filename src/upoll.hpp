3f030202f2fe5ebf69b02c886512b6781c8e4f55
Martin Sustrik <sustrik@250bpm.com>
2012-02-16 10:09:44 +0900

    xs_poll implementation moved to a separate file
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

---
+++ src/upoll.hpp
@@ -0,0 +1,32 @@
+/*
+    Copyright (c) 2012 250bpm s.r.o.
+    Copyright (c) 2012 Other contributors as noted in the AUTHORS file
+
+    This file is part of Crossroads project.
+
+    Crossroads is free software; you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Crossroads is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __XS_POLLING_HPP_INCLUDED__
+#define __XS_POLLING_HPP_INCLUDED__
+
+#include "../include/xs.h"
+
+namespace xs
+{
+    //  Underlying function for xs_poll.
+    int upoll (xs_pollitem_t *items_, int nitems_, long timeout_);
+}
+
+#endif
